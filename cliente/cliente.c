f414360f00191c94cca045b70d4211ef83aea49a
Luis Martinez <s_k_ap3@hotmail.com>
2021-02-24 20:39:31 -0600

    colocando cabeceras faltantes

--- cliente/cliente.c
+++ cliente/cliente.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
- 
+#include <string.h>
+#include <unistd.h>
+
 int main(int argc, char *argv[]){
  
       if(argc > 2){
             char *ip;
             int fd, numbytes,puerto;
             char buf[100];
-            puerto=atoi(argv[2]);
-            ip=argv[1];
+            puerto = atoi(argv[2]);
+            ip = argv[1];
 
             struct hostent *he;
             struct sockaddr_in server;
 
-            if ((he=gethostbyname(ip))==NULL){
+            if ((he = gethostbyname(ip)) == NULL){
                   printf("gethostbyname() error\n");
-                  exit(-1);
+                  exit(0);
             }
 
-            if ((fd=socket(AF_INET, SOCK_STREAM, 0))==-1){
+            if ((fd=socket(AF_INET, SOCK_STREAM, 0)) == -1){
                   printf("socket() error\n");
-                  exit(-1);
+                  exit(0);
             }
 
             server.sin_family = AF_INET;
@@ -31,17 +34,17 @@ int main(int argc, char *argv[]){
             server.sin_addr = *((struct in_addr *)he->h_addr);
             bzero(&(server.sin_zero),8);
 
-            if(connect(fd, (struct sockaddr *)&server,sizeof(struct sockaddr))==-1){
+            if(connect(fd, (struct sockaddr *)&server,sizeof(struct sockaddr)) == -1){
                   printf("connect() error\n");
-                  exit(-1);
+                  exit(0);
             }
 
             if ((numbytes=recv(fd,buf,100,0)) == -1){
                   printf("Error en recv() \n");
-                  exit(-1);
+                  exit(0);
             }
 
-            buf[numbytes]='\0';
+            buf[numbytes] = '\0';
 
             printf("Mensaje del Servidor: %s\n",buf);
 
